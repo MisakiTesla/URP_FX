3c2baf9bbe3e6101fc97b2532e980eea8a5d04de
Miha <619231532@qq.com>
2022-07-16 22:58:26 +0800

    +Simple Scanner

---
+++ Assets/Scanner/Scanner.cs
@@ -0,0 +1,143 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class Scanner : ScriptableRendererFeature
+{
+    public Material material;//使用的材质
+    public Color color;
+    public Vector3 centerPos;
+    public float radius;
+    public float width;
+    public float bias;
+    public RenderPassEvent passEvent = RenderPassEvent.AfterRenderingTransparents;        //默认在transparent后
+    
+    class ScannerPass : ScriptableRenderPass
+    {
+        private Material _material;
+        
+        public ScannerPass(Material material)
+        {
+            _material = material;
+        }
+        
+        public void SetUp(RenderTargetIdentifier colorIdentifier)
+        {
+        }
+        
+        // This method is called before executing the render pass.
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in a performant manner.
+        public override void OnCameraSetup(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            // ConfigureTarget();
+            // Debug.Log("OnCameraSetup");
+            // _depthId = renderingData.cameraData.renderer.cameraDepthTarget;
+
+            // ConfigureTarget(renderingData.cameraData.renderer.cameraColorTarget, desc.depthStencilFormat, desc.width, desc.height, 1, true);
+            ConfigureTarget(renderingData.cameraData.renderer.cameraColorTarget);
+
+
+            
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmd = CommandBufferPool.Get("Depth2WorldPos");
+            
+            int tempRT = Shader.PropertyToID("Depth2WorldPosTempRT");
+            RenderTextureDescriptor desc = renderingData.cameraData.cameraTargetDescriptor;
+            // Debug.Log();
+            // 1 射线法
+            // 相似三角形
+            // 构建camera到 深度的射线向量，存储在矩阵里，用于重建世界坐标
+            {
+                Camera cam = renderingData.cameraData.camera;
+
+                float height = cam.nearClipPlane * Mathf.Tan(Mathf.Deg2Rad * cam.fieldOfView * 0.5f);
+                Vector3 up = cam.transform.up * height;
+                Vector3 right = cam.transform.right * height * cam.aspect;
+                Vector3 forward = cam.transform.forward * cam.nearClipPlane;
+                Vector3 ButtomLeft = forward - right - up;//camera到near平面四角的 向量
+
+                float scale = ButtomLeft.magnitude / cam.nearClipPlane;
+
+                ButtomLeft.Normalize();
+                ButtomLeft *= scale;
+                Vector3 ButtomRight = forward + right - up;
+                ButtomRight.Normalize();
+                ButtomRight *= scale;
+                Vector3 TopRight = forward + right + up;
+                TopRight.Normalize();
+                TopRight *= scale;
+                Vector3 TopLeft = forward - right + up;
+                TopLeft.Normalize();
+                TopLeft *= scale;
+
+                Matrix4x4 MATRIX = new Matrix4x4();
+
+                MATRIX.SetRow(0, ButtomLeft);
+                MATRIX.SetRow(1, ButtomRight);
+                MATRIX.SetRow(2, TopRight);
+                MATRIX.SetRow(3, TopLeft);
+
+                _material.SetMatrix("Matrix", MATRIX);
+            }
+            
+
+            
+            cmd.GetTemporaryRT(tempRT, desc);
+            cmd.Blit(colorAttachment, tempRT, _material,0);
+            // cmd.Blit(colorAttachment, tempRT);
+            cmd.Blit(tempRT, colorAttachment);
+            context.ExecuteCommandBuffer(cmd);
+
+            CommandBufferPool.Release(cmd);
+            cmd.ReleaseTemporaryRT(tempRT);
+
+        }
+
+        // Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+        }
+    }
+
+    ScannerPass _scannerPass;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        _scannerPass = new ScannerPass(material);
+
+        // Configures where the render pass should be injected.
+        _scannerPass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+    }
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        Debug.Log("AddRenderPasses");
+        if (material)
+        {
+            //设置参数
+            material.SetColor("_Color", color);
+            material.SetVector("_CenterPos", centerPos);
+            material.SetFloat("_Radius", radius);
+            material.SetFloat("_Width", width);
+            material.SetFloat("_Bias", bias);
+
+            renderer.EnqueuePass(_scannerPass);
+        }
+    }
+}
+
+
---
+++ Assets/Scanner/Scanner.shader
@@ -0,0 +1,142 @@
+Shader "URP_FX/Scanner"
+{
+    Properties
+    {
+        //[KeywordEnum(X,Y,Z)]_AXIS("Axis",float)=1
+        _MainTex("MainTex",2D)= "white"{}
+        _Color("Color",color)=(0,0,0,0)
+        _CenterPos("CenterPos",vector)=(0,0,0,0)
+        _Radius("Radius",float)=0
+        _Width("Width",float)=0
+        _Bias("Bias",float)=0
+    }
+
+    SubShader
+    {
+        Tags
+        {
+            "RenderPipeline"="UniversalRenderPipeline"
+            "Queue"="Transparent"
+        }
+        
+//        Blend SrcAlpha OneMinusSrcAlpha
+
+        Cull Off ZWrite Off ZTest Always
+
+        HLSLINCLUDE
+        #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+        #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+
+        CBUFFER_START(UnityPerMaterial)
+
+        float4 _MainTex_ST;
+        float4 _MainTex_TexelSize;
+
+        CBUFFER_END
+        
+
+
+        TEXTURE2D(_MainTex);
+        SAMPLER(sampler_MainTex);
+        
+        float4x4 Matrix;
+        
+        float4 _Color;
+        float4 _CenterPos;
+        float _Radius;
+        float _Width;
+        float _Bias;
+
+        struct a2v
+        {
+            float4 positionOS:POSITION;
+            float2 texcoord:TEXCOORD;
+        };
+
+        struct v2f
+        {
+            float4 positionCS:SV_POSITION;
+            float2 texcoord:TEXCOORD;
+            float3 Dirction:TEXCOORD1;
+        };
+        ENDHLSL
+
+        pass
+        {
+
+            HLSLPROGRAM
+            #pragma vertex VERT
+            #pragma fragment FRAG
+            #pragma multi_compile_local _AXIS_X _AXIS_Y _AXIS_Z
+
+            v2f VERT(a2v i)
+            {
+                v2f o;
+                o.positionCS = TransformObjectToHClip(i.positionOS.xyz);
+                o.texcoord = i.texcoord;
+
+                int t;
+                if (i.texcoord.x < 0.5 && i.texcoord.y < 0.5)
+                    t = 0;
+                else if (i.texcoord.x > 0.5 && i.texcoord.y < 0.5)
+                    t = 1;
+                else if (i.texcoord.x > 0.5 && i.texcoord.y > 0.5)
+                    t = 2;
+                else
+                    t = 3;
+                //这个Direction从VS到FS会被线性插值，所以在FS中每个片元就可以用其LinearEyeDepth乘以这个方向再加上相机的世界坐标来重建世界坐标
+                o.Dirction = Matrix[t].xyz;
+                return o;
+            }
+
+            int sobel(v2f i);
+
+            real4 FRAG(v2f i):SV_TARGET
+            {
+                real depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture, sampler_CameraDepthTexture, i.texcoord);
+                // return depth;
+                
+                // 1.射线法 性能最好
+                // float linearEyeDepth = LinearEyeDepth(depth,_ZBufferParams);
+                // float3 WSpos = _WorldSpaceCameraPos + linearEyeDepth * i.Dirction;
+                // return real4(WSpos.xyz,1);
+
+                // 2. ndc重建法 1.手动
+                // float4 ndc = float4(i.texcoord.x*2 - 1, i.texcoord.y * 2 - 1, depth, 1);
+                // #if UNITY_UV_STARTS_AT_TOP
+                // ndc.y = -ndc.y;
+                // #endif
+                // float4 World= mul(UNITY_MATRIX_I_VP, ndc);
+                // World.xyz = World.xyz/ World.w;
+                // return real4(World.xyz,1);
+
+                //             2.现成的轮子
+                float3 worldPos = ComputeWorldSpacePosition(i.texcoord, depth, UNITY_MATRIX_I_VP);
+                // return float4(worldPos.xyz,1);
+
+                // 3. ndc重建 ASE版
+                // #if UNITY_REVERSED_Z
+                // depth = 1.0 - depth;
+                // #endif
+                //
+                // float4 ndcPos = float4(i.texcoord.x * 2 - 1, i.texcoord.y * 2 - 1, depth * 2 - 1, 1); //NDC坐标
+                // float4 H = mul(unity_CameraInvProjection, ndcPos);
+                // float4 V = float4((H.xyz/H.w) * float3(1,1,-1) , 1.0);
+                // float4 W = mul(unity_CameraToWorld, V);
+                // return float4(W.xyz,1);
+
+                float4 sceneColor = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, i.texcoord);
+
+                float lengthToCenter = distance(worldPos, _CenterPos.xyz);
+                // return sceneColor + lengthToCenter<3;
+                float scannerNearLine = smoothstep(_Radius - _Width - _Bias, _Radius - _Width, lengthToCenter);
+                float scannerFarLine = smoothstep(_Radius + _Width - _Bias, _Radius + _Width, lengthToCenter);
+                return sceneColor + (scannerNearLine - scannerFarLine)*_Color;
+            }
+            
+            ENDHLSL
+
+        }
+
+    }
+}
